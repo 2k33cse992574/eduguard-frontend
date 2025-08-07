a1b4b4465ce20c517eb1e33bc3394d6a1e09a5ec
Arin Sharma <sharmaarin87@email.com>
2025-08-07 19:40:21 +0530

    🔧 Fixed API endpoint in script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ form.addEventListener("submit", async (e) => {
   const formData = new FormData(form);
 
   try {
-    const res = await fetch("https://exam-wsta.onrender.com", {
+    const res = await fetch("https://exam-wsta.onrender.com/api/reports", {
       method: "POST",
       body: formData,
     });
